937fa6b2924ba3aebe46ec18c916bc1f43e30bc1
juliokval <julesvalentin04@gmail.com>
2022-06-29 17:55:59 +0000

    From 0-all

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+* insert_node - inserts number in sinfly linked list
+* @head: head
+* @number: number to insert
+* Return: Null if failure, else a pointer to the new node
+*otherwise a pointer to the new node
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+listint_t *node = *head, *new;
+
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+return (NULL);
+new->n = num;
+if (node == NULL || node->n >= num)
+{
+new->next = node;
+*head = new;
+return (new);
+}
+while (node && node->next && node->next->n < num)
+node = node->next;
+new->next = node->next;
+node->next = new;
+return (new);
+}
